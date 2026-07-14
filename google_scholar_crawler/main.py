b8d77b80e6131ad8356c332788c47a5bb149fffc
Yue Li <75842784+TheShineyue@users.noreply.github.com>
2026-07-14 20:14:41 +0800

    Update main.py

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -1,28 +1,28 @@
-from scholarly import scholarly, ProxyGenerator  # ← 新增 ProxyGenerator
+from scholarly import scholarly
 import jsonpickle
 import json
 from datetime import datetime
 import os
 
-pg = ProxyGenerator()
-pg.FreeProxies()
-scholarly.use_proxy(pg)
-
-author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+# 直接查询，不用代理
+author = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
 scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+
 name = author['name']
 author['updated'] = str(datetime.now())
-author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+author['publications'] = {v['author_pub_id']: v for v in author['publications']}
+
 print(json.dumps(author, indent=2))
+
 os.makedirs('results', exist_ok=True)
-with open(f'results/gs_data.json', 'w') as outfile:
+with open('results/gs_data.json', 'w') as outfile:
     json.dump(author, outfile, ensure_ascii=False)
 
 shieldio_data = {
-  "schemaVersion": 1,
-  "label": "citations",
-  "message": f"{author['citedby']}",
+    "schemaVersion": 1,
+    "label": "citations",
+    "message": f"{author['citedby']}",
 }
 
-with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
+with open('results/gs_data_shieldsio.json', 'w') as outfile:
     json.dump(shieldio_data, outfile, ensure_ascii=False)
